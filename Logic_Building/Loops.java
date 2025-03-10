0dedc5554bc751c03fc34174056fc2b05a0e81a8
RishitaCS <rishitarawat1000@gmail.com>
2025-03-10 13:10:32 +0530

    Loops

---
+++ Logic_Building/Loops.java
@@ -0,0 +1,40 @@
+package Logic_Building;
+
+public class Loops {
+    public static void main(String[] args) {
+        
+        // while
+        /*int i=1; //start
+        while(i<=10){ //condition
+            System.out.println("Rishita");
+        }
+        //i = i+1;
+        i++; //increment
+        */
+
+        // for
+        /*for(int i=1; i<=10; i++){
+            System.out.println("Rishita");
+        }*/
+
+        // Do-while
+        /*int i = 20;
+        do{
+            System.out.println("Rishita");
+            i++;
+        }while(i<=10);
+        */
+
+        //Java 5 onwards
+        //Enhance for loop
+        int arr[] = {10,20,30,40,50};
+        // Traditional Way
+        // for(int i=0; i<arr.length; i++){
+        //     System.out.println(arr[i]);
+        // }
+
+        for(int e:arr){
+            System.out.println(e);
+        }
+    }
+}
