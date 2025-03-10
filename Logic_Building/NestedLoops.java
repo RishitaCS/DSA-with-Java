562b8e0d0eb70a715d972e2530499abb078ea57e
RishitaCS <rishitarawat1000@gmail.com>
2025-03-10 21:43:16 +0530

    Nested loops

---
+++ Logic_Building/NestedLoops.java
@@ -0,0 +1,11 @@
+package Logic_Building;
+
+public class NestedLoops {
+    public static void main(String[] args) {
+        for(int i=1; i<=3; i++){ //outer loop
+            for(int j=1; j<=3; j++){ //inner loop
+                System.out.println(i +" "+j);
+            }
+        }
+    }
+}
