85289a7fe94f0c1d5bffb1c2987c7df39e8e630b
RishitaCS <rishitarawat1000@gmail.com>
2025-03-10 21:43:07 +0530

    Labeled loops

---
+++ Logic_Building/LabeledLoops.java
@@ -0,0 +1,18 @@
+package Logic_Building;
+
+public class LabeledLoops {
+    public static void main(String[]args){
+        outer:
+        for(int i=1; i<=3; i++){ //outer loop
+            for(int j=1; j<=3; j++){ 
+                if(i==j){
+                    // return does not work with labels
+                    continue outer; // skip the labeled iteration
+                    //break outer; // exit from the outer (labeled) loop
+                }
+                System.out.println(i + " " +j);
+            }
+        }
+        System.out.println("exit from the loops");
+    }
+}
