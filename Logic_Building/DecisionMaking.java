6d0caaeba32ebb79a82dd767cf4f52d8f37d6645
RishitaCS <rishitarawat1000@gmail.com>
2025-03-10 13:10:00 +0530

    Decision making

---
+++ Logic_Building/DecisionMaking.java
@@ -0,0 +1,40 @@
+package Logic_Building;
+
+public class DecisionMaking {
+    public static void main(String[] args) {
+        int num = 20;
+        // Simple if-else
+        if(num%2==0){ //true
+            System.out.println("Even number");
+        }
+        else{ //false
+            System.out.println("Odd number");
+        }
+
+        /*Multiple if-else
+         * if(){
+         * 
+         * }
+         * else if(){
+         * 
+         * }
+         * else{
+         * 
+         * }
+         */
+
+         /*Nested if-else
+          * if(){ //outer if
+             if(){ //inner if
+                  code
+             }
+             else{ //inner else
+
+             }
+          * }
+            else{ //outer else
+            
+            }
+          */
+    }
+}
