303c84a175c0c96ea446b8eb0056d4b2237e6b85
RishitaCS <rishitarawat1000@gmail.com>
2025-03-10 21:42:52 +0530

    Break and continue

---
+++ Logic_Building/BreakVsContinue.java
@@ -0,0 +1,16 @@
+package Logic_Building;
+
+public class BreakVsContinue {
+    public static void main(String[] args) {
+        for(int i=1; i<=3; i++){ //outer loop
+            for(int j=1; j<=3; j++){ 
+                if(i==j){
+                    return; // exit from the current function
+                    //continue; // continue - skip or exit from the current iteration
+                    //break; // break - exit from the current loop
+                }
+                System.out.println(i + " " +j);
+            }
+        }
+    }
+}
