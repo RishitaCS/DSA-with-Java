756e678550df2c3e7bc38f013a19f4cf1ba4dab1
RishitaCS <rishitarawat1000@gmail.com>
2025-03-21 23:51:46 +0530

    Tree Recursion and one Pattern question with recursion

---
+++ Recursion/FiboWithTreeRecursion.java
@@ -0,0 +1,17 @@
+package Recursion;
+// Finding 8th term
+public class FiboWithTreeRecursion {
+
+    static int fibo(int num){
+        if(num<=1){
+            return num;
+        }
+        int firstTerm = fibo(num-1);
+        int secondTerm = fibo(num-2);
+        return firstTerm + secondTerm;
+    }
+    public static void main(String[] args) {
+        int result = fibo(8);
+        System.out.println(result);
+    }
+}
---
+++ Recursion/TreeRecursion.java
@@ -0,0 +1,18 @@
+package Recursion;
+
+public class TreeRecursion {
+    static void fun(int num){
+        // Base case
+        if(num<=0){
+            return;
+        }
+        System.out.println("Pre Call " + num);
+        fun(num-1); // Recursive call
+        System.out.println("In b/w Call " + num);
+        fun(num-2); // Recursive call
+        System.out.println("Post Call " + num);
+    }
+    public static void main(String[] args) {
+        fun(3);
+    }
+}
